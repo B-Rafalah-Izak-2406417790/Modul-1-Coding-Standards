13345f848c6b001c7979617d249b0ab138288a88
Rafalah7 <rafalahizak@gmail.com>
2026-02-08 23:07:41 +0700

    fixing build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 val seleniumJavaVersion = "4.14.1"
 val seleniumJupiterVersion = "5.0.1"
 val webdrivermanagerVersion = "5.6.3"
-val junitJupiterVersion = "5.9.1"
 
 plugins {
 	java
@@ -38,11 +37,11 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
-	testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
-	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+	testImplementation("org.seleniumhq.selenium:selenium-java:${seleniumJavaVersion}")
+	testImplementation("io.github.bonigarcia:selenium-jupiter:${seleniumJupiterVersion}")
+	testImplementation("io.github.bonigarcia:webdrivermanager:${webdrivermanagerVersion}")
+	testImplementation("org.junit.jupiter:junit-jupiter-api")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
 tasks.register<Test>("unitTest") {
